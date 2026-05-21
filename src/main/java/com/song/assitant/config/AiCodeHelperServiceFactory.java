7da6259b0df54023f2c633a00f6ae827f30288d5
yu <3248856990@qq.com>
2026-05-21 17:13:06 +0800

    feat:采用整合依赖自动创建Service(不推荐这种写法)

--- src/main/java/com/song/assitant/config/AiCodeHelperServiceFactory.java
+++ src/main/java/com/song/assitant/config/AiCodeHelperServiceFactory.java
@@ -3,6 +3,7 @@ package com.song.assitant.config;
 import com.song.assitant.ai.AiCodeHelperService;
 import dev.langchain4j.model.chat.ChatModel;
 import dev.langchain4j.service.AiServices;
+import dev.langchain4j.service.spring.AiService;
 import jakarta.annotation.Resource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,15 +14,11 @@ import org.springframework.context.annotation.Configuration;
  * @description AiService创建的工厂类
  * @date 2026/5/21
  */
-@Configuration
+@AiService
 public class AiCodeHelperServiceFactory {
 
     @Resource
     private ChatModel qwenChatModel;
 
-    @Bean
-    public AiCodeHelperService aiCodeHelperService() {
-        // create方法提供了创建AiService的类别 原理是通过java的反射和动态代理机制完成AiService的创建
-        return AiServices.create(AiCodeHelperService.class, qwenChatModel);
-    }
+
 }
