4eab3c2c8d5c86fdf1aa2c375580434f4b3d4347
yu <3248856990@qq.com>
2026-05-21 17:22:01 +0800

    feat:会话记忆实现

---
+++ src/main/java/com/song/assitant/config/AiCodeHelperFactory.java
@@ -0,0 +1,33 @@
+package com.song.assitant.config;
+
+import com.song.assitant.ai.AiCodeHelperService;
+import dev.langchain4j.memory.ChatMemory;
+import dev.langchain4j.memory.chat.MessageWindowChatMemory;
+import dev.langchain4j.model.chat.ChatModel;
+import dev.langchain4j.service.AiServices;
+import jakarta.annotation.Resource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author huang
+ * @version 1.0
+ * @description 会话记忆实现
+ * @date 2026/5/21
+ */
+@Configuration
+public class AiCodeHelperFactory {
+    @Resource
+    private ChatModel qwenChatModel;
+
+    @Bean
+    public AiCodeHelperService aiCodeHelperService() {
+        // 会话记忆
+        ChatMemory chatMemory = MessageWindowChatMemory.withMaxMessages(10);
+        AiCodeHelperService aiCodeHelperService = AiServices.builder(AiCodeHelperService.class)
+                .chatModel(qwenChatModel)
+                .chatMemory(chatMemory)
+                .build();
+        return aiCodeHelperService;
+    }
+}
