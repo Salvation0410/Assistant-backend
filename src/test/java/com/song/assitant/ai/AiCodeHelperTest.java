33ba6aaf3abac66d8cfbc7ec8d415d70d81cb7bf
yu <3248856990@qq.com>
2026-05-21 16:54:03 +0800

    feat:普通对话 涉及多模态的对话

---
+++ src/test/java/com/song/assitant/ai/AiCodeHelperTest.java
@@ -0,0 +1,40 @@
+package com.song.assitant.ai;
+
+import dev.langchain4j.data.message.ImageContent;
+import dev.langchain4j.data.message.TextContent;
+import dev.langchain4j.data.message.UserMessage;
+import jakarta.annotation.Resource;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author huang
+ * @version 1.0
+ * @description
+ * @date 2026/5/21
+ */
+
+
+@SpringBootTest
+class AiCodeHelperTest {
+
+    @Resource
+    private AiCodeHelper aiCodeHelper;
+
+    @Test
+    void chat() {
+        aiCodeHelper.chat("你好，我是关羽");
+    }
+    // 多模态测试
+    @Test
+    void chatWithMessage() {
+        UserMessage userMessage = UserMessage.from(
+                TextContent.from("描述图片"),
+                ImageContent.from("https://www.codefather.cn/logo.png")
+        );
+        aiCodeHelper.chatWithMessage(userMessage);
+    }
+
+}
