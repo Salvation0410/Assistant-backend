e563367ce54d7ccc114e5bfefc74aa88344f206d
yu <3248856990@qq.com>
2026-05-22 09:44:18 +0800

    feat:结构化输出

--- src/main/java/com/song/assitant/ai/AiCodeHelperService.java
+++ src/main/java/com/song/assitant/ai/AiCodeHelperService.java
@@ -4,13 +4,23 @@ import dev.langchain4j.service.MemoryId;
 import dev.langchain4j.service.SystemMessage;
 import dev.langchain4j.service.UserMessage;
 
+import java.util.List;
+
 /**
  * @author huang
  * @version 1.0
  * @description AiService功能使用
  * @date 2026/5/21
  */
+
+
 public interface AiCodeHelperService {
     @SystemMessage(fromResource = "system-prompt.txt")
-    String chat(@MemoryId Integer memoryId,@UserMessage String userMessage);
+    String chat(String userMessage);
+
+    @SystemMessage(fromResource = "system-prompt.txt")
+    Record chatForReport(String userMessage);
+
+
+    record Record(String name, List<String> suuggestionList){}
 }
--- src/main/java/com/song/assitant/config/AiCodeHelperServiceFactory.java
+++ src/main/java/com/song/assitant/config/AiCodeHelperServiceFactory.java
@@ -3,7 +3,6 @@ package com.song.assitant.config;
 import com.song.assitant.ai.AiCodeHelperService;
 import dev.langchain4j.model.chat.ChatModel;
 import dev.langchain4j.service.AiServices;
-import dev.langchain4j.service.spring.AiService;
 import jakarta.annotation.Resource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,11 +13,15 @@ import org.springframework.context.annotation.Configuration;
  * @description AiService创建的工厂类
  * @date 2026/5/21
  */
-@AiService
+//@Configuration
 public class AiCodeHelperServiceFactory {
 
     @Resource
     private ChatModel qwenChatModel;
 
-
+    @Bean
+    public AiCodeHelperService aiCodeHelperService() {
+        // create方法提供了创建AiService的类别 原理是通过java的反射和动态代理机制完成AiService的创建
+        return AiServices.create(AiCodeHelperService.class, qwenChatModel);
+    }
 }
